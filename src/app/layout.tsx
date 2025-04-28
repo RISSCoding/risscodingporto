36939e233fad67c823873910432b27bb95fb3b54
RISSCoding <farisfebiran0602@gmail.com>
2025-04-28 18:36:07 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Calistoga } from "next/font/google";
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans'});
 const calistoga = Calistoga({ subsets: ['latin'], variable: '--font-serif', weight: ['400']});
@@ -48,6 +49,7 @@ export default function RootLayout({
       </head>
       <body className={twMerge(inter.variable, calistoga.variable, "bg-gray-900 text-white antialiased font-sans")}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
